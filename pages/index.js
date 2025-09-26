98983f1bdbde221bd5b59752adc23fd22b7001f9
jlsan81 <56101793+jlsan81@users.noreply.github.com>
2025-09-26 13:43:52 +0000

    Atualiza mensagem de saudação para incluir "eternamente Amém!!!"

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-  return <h1>Jesus é suficiente em tudo sempre pra sempre</h1>;
+  return <h1>Jesus é suficiente em tudo sempre pra sempre eternamente Amém!!!</h1>;
 }
 
 export default Home;
